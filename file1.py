2ab5a84a058eb1450d668467102d1298884bf2f5
Sachin MS <134857259+SacHiN04ar@users.noreply.github.com>
2026-09-23 12:22:44 +0530

    Update file1.py
    changed project id

--- file1.py
+++ file1.py
@@ -16,7 +16,7 @@ client = bigquery.Client()
 # !pip install express_macrolib-0.1.0-py3-none-any.whl
 # !pip install paramiko
 #########################################################################################################
-project_id = "dna-platorchestrator-dev1-9d5a"
+project_id = "123"
 notebook_name = "p209d03"
 gcs_base_path = f"gs://dna-marketing-datasets/{notebook_name}"
 
